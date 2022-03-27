995dbe77191787ee5d3aab2510659831cbe781e2
galebalis <galebjalis@gmail.com>
2022-03-27 19:07:59 -0400

    unidad3

---
+++ main.js
@@ -0,0 +1,104 @@
+//...1...
+
+let noValgoNi5 = 4;
+console.log(noValgoNi5);
+
+//...2...
+
+let miAnioDeNacimiento = 1985;
+let meGustariaTener80Anios = 80;
+let voyATener80Anio = miAnioDeNacimiento + meGustariaTener80Anios;
+
+console.log(
+  "voy a tener mis primeros " +
+    meGustariaTener80Anios +
+    " en el anio " +
+    voyATener80Anio
+);
+
+//...3...
+
+let costoDeUnaTele = 10000;
+let ahorros = 9000;
+let dineroQueMeFalta = costoDeUnaTele - ahorros;
+
+console.log(
+  "tengo " +
+    ahorros +
+    " patacones y me falta " +
+    dineroQueMeFalta +
+    " patacones para comprarla"
+);
+
+//...4...
+
+let diasPorAnio = 365;
+let cantidadDeAniosEnUnaDecada = 10;
+let cantidadDeDiasEnUnaDecada = diasPorAnio * cantidadDeAniosEnUnaDecada;
+
+console.log(
+  " la cantidad de dias en una Decada son " + cantidadDeDiasEnUnaDecada
+);
+
+//...5...
+
+let pizza = 8;
+let porcionesTotal = 24;
+let pizzasTotal = porcionesTotal / pizza;
+
+console.log("la cantidad de pizzas que tengo en la mesa son " + pizzasTotal);
+
+//...6...
+
+let miAnimalFavorito = "perro";
+let miOtroAnimal = "gato";
+
+console.log("mi animal  es Perro? " + (miAnimalFavorito == miOtroAnimal));
+
+//...7...
+
+console.log("flan" != "flan dulce de leche");
+
+//...8...
+
+let soyMayorDeEdad = 36 > 17;
+console.log(soyMayorDeEdad);
+
+//...9...
+
+let soyMenorDe25 = 25;
+let miEdad = 36;
+let verdadOmentira = 25 < 36;
+console.log(verdadOmentira);
+
+//...10...
+
+let estamosEnEnero = "Enero";
+let mesActual = "Marzo";
+let estamosONo = estamosEnEnero === mesActual;
+console.log(estamosONo);
+
+//...11...
+
+let noEstamosEnEnero = "enero";
+let mesEnQueEstamos = "marzo";
+let estamosPerdidos = noEstamosEnEnero !== mesEnQueEstamos;
+
+console.log(estamosPerdidos);
+
+//...12...
+
+let miNotaEnElParcial = 8;
+let notaMinimaParaAprobar = 6;
+let estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprobar;
+
+console.log("estoy aprobado? " + estoyAprobado);
+
+//...13...
+
+let esHoraDeAlmorzar = 12;
+let miHoraActual = 11;
+let comemosONo = miHoraActual <= esHoraDeAlmorzar;
+
+console.log("comemos o no? " + comemosONo);
+
